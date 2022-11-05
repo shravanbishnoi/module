63e87f49cb75b6228a7e875014ee6e877f7bf228
Shravan Bishnoi <116088301+shravanbishnoi@users.noreply.github.com>
2022-11-05 22:00:10 +0530

    Create module1.py

---
+++ module1.py
@@ -0,0 +1,11 @@
+'''First code for module,
+this is very simple code for beginner to 
+understand the concept of modules then I have 
+user built function in it to explore more in it.'''
+x=1+2
+x=3*x
+x
+def greet(n):
+	text="Hello " +n+"!"
+	print(text)
+	
